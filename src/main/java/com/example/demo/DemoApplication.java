02219c3a907709ea8c56a3fc832b4881578b3115
천광규 <kk.chon@tg360tech.com>
2022-05-20 14:55:01 +0900

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,26 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @Slf4j
-public class DemoApplication {
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void init(){
+	public void initialize(ApplicationReadyEvent event){
 		log.info("Hello EventListener!! ApplicationReadyEvent");
+		System.out.println("App Args: " + Arrays.asList(event.getArgs()));
 	}
-
 }
